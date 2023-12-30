f649e09138d9a968e5d522ecb784b991b68b6046
elkitty <hablaconwafle@gmail.com>
2023-12-30 13:00:54 -0300

    first commit: env setup, clek auth, modal components, form components,  planetscale, mysql setup, dashboard setup

---
+++ components/modals/store-modal.tsx
@@ -0,0 +1,96 @@
+"use client"
+
+import * as z from "zod"
+import {useForm} from "react-hook-form"
+import { useState } from "react";
+import axios from "axios";
+import toast from "react-hot-toast";
+
+import { useStoreModal } from "@/hooks/use-store-modal";
+import { Modal } from "@/components/ui/modal";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "../ui/form";
+import { Input } from "../ui/input";
+import { Button } from "../ui/button";
+
+
+
+
+const formSchema = z.object({
+    name: z.string().min(1),
+})
+
+export const StoreModal = () => {
+    const storeModal = useStoreModal();
+
+    const [loading, setLoading] =  useState(false);
+
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            name: "",
+        },
+    })
+
+    const onSubmit = async (values: z.infer<typeof formSchema>) => {
+            try {
+                setLoading(true)
+                const response = await axios.post('/api/stores', values);
+                window.location.assign(`/${response.data.id}`)
+            } catch (error) {
+                toast.error("Something went wrong.")
+            } finally {
+                setLoading(false);
+            }
+    }
+    return (
+        <Modal
+            title="Create store"
+            description="Adding a new Store"
+            isOpen={storeModal.isOpen}
+            onClose={storeModal.onClose}
+            >
+            <div>
+                <div className="space-y4 py-2 pb-4">
+                    <Form {...form}>
+                        <form onSubmit={form.handleSubmit(onSubmit)}>
+                            <FormField
+                                control={form.control}
+                                name="name"
+                                render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>Name</FormLabel>
+                                <FormControl>
+                                    <Input 
+                                    disabled={loading} 
+                                    placeholder="E-Commerce Name" {...field}/>
+                                </FormControl>
+                                <FormMessage/>
+                            </FormItem>)}
+                            />
+                            <div className="pt-6 space-x-2 flex items-center justify-end w-full">
+                                    <Button
+                                        disabled={loading}
+                                        variant="outline"
+                                        onClick={storeModal.onClose}
+                                    >
+                                        Cancel
+                                    </Button>
+                                    <Button
+                                        disabled={loading} 
+                                        type="submit"
+                                    >
+                                        Continue
+                                    </Button>
+                            </div>
+                        </form>
+                    </Form>
+                </div>
+            </div>
+        </Modal>
+    )
+}
+
+
+
+
---
+++ components/navbar.tsx
@@ -0,0 +1,14 @@
+const Navbar = () => {
+    return (
+        <div className="border-b">
+            <div className="flex h-16 items-center px-4">
+                <div>
+                    This will be a store switcher
+                </div>
+
+            </div>
+        </div>
+    )
+}
+
+export default Navbar;
