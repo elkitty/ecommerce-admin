2a26d7c2dbc6c9e7da9fa750d64c1053ff1967d2
elkitty <hablaconwafle@gmail.com>
2024-01-04 01:01:44 -0300

    added navigation bar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,9 +1,30 @@
-const Navbar = () => {
+import { UserButton, auth } from "@clerk/nextjs";
+
+import { MainNav } from "@/components/main-nav"
+import StoreSwitcher from "./store-switcher";
+import { redirect } from "next/navigation";
+import prismadb from "@/lib/prismadb";
+
+const Navbar = async  () => {
+    const { userId } = auth();
+
+    if (!userId) {
+        redirect("/sign-in");
+    }
+
+    const stores = await prismadb.store.findMany({
+        where: { 
+            userId,
+        },
+    })
+ 
     return (
         <div className="border-b">
             <div className="flex h-16 items-center px-4">
-                <div>
-                    This will be a store switcher
+                <StoreSwitcher items={stores} />
+                <MainNav/>
+                <div className="ml-auto flex items-center space-x-4">
+                    <UserButton afterSignOutUrl="/"/>
                 </div>
 
             </div>
